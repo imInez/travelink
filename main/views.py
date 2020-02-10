b77e044c386d4c8e968cd36b8a8cacd586f8d1a8
Inez <iminezj@gmail.com>
2020-02-10 20:46:07 +0100

    Added pagination

--- main/views.py
+++ main/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from .models import Travel
-# Create your views here.
+from django.core.paginator import Paginator
+from django.views.generic import ListView
+
+
 
 def home(request):
-    context = {
-        'travels' : Travel.objects.all().order_by('-added'),
-    }
-    return render(request, 'main/index.html', context)
+    p = Paginator(Travel.objects.all().order_by('added'), 18)
+    page_number = request.GET.get('page')
+    page_obj = p.get_page(page_number)
+    return render(request, 'main/index.html', {'page_obj': page_obj})
