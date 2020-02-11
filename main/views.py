9097ef96d18cd4a19834ea0d2cd42ed10cf352b9
Inez <iminezj@gmail.com>
2020-02-11 21:39:50 +0100

    Changed ordering of travels

--- main/views.py
+++ main/views.py
@@ -6,7 +6,7 @@ from django.views.generic import ListView
 
 
 def home(request):
-    p = Paginator(Travel.objects.all().order_by('added'), 18)
+    p = Paginator(Travel.objects.all().order_by('-added'), 18)
     page_number = request.GET.get('page')
     page_obj = p.get_page(page_number)
     return render(request, 'main/index.html', {'page_obj': page_obj})
